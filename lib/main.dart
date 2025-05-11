2e5818aa5696c9dedb4340a8f03d48f7d8c3ebfe
David <kouchdavid78@gmail.com>
2025-05-11 14:08:22 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(const MyApp());
@@ -28,6 +29,38 @@ class MyAppContribute extends StatefulWidget {
 }
 
 class MyAppBody extends State<MyAppContribute>{
+
+  var rng = new Random();
+  var init = 0;
+  var ran1 = ["blank","bongkong","kdam","kla","klok","morn","trey"];
+  var ran2 = ["blank","bongkong","kdam","kla","klok","morn","trey"];
+  var ran3 = ["blank","bongkong","kdam","kla","klok","morn","trey"];
+
+  int ran1Index = 2;
+  int ran2Index = 2;
+  int ran3Index = 2;
+
+  void RandomNumber() {
+    setState(() {
+      ran1Index = rng.nextInt(6) + 1;
+      ran2Index = rng.nextInt(6) + 1;
+      ran3Index = rng.nextInt(6) + 1;
+
+
+      // ran1 = rng.nextInt(6);
+      // ran2 = rng.nextInt(6);
+      // ran3 = rng.nextInt(6);
+    });
+  }
+
+  void restart() {
+    setState(() {
+      ran1Index = 0;
+      ran2Index = 0;
+      ran3Index = 0;  
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -39,7 +72,7 @@ class MyAppBody extends State<MyAppContribute>{
         appBar: AppBar(
           centerTitle: true,
           title: const Text(
-            'ល្បែងខ្លាខ្លោក',
+            'ល្បែងខ្លាឃ្លោក',
             style: TextStyle(
               fontSize: 20,
               color: Colors.white,
@@ -48,17 +81,92 @@ class MyAppBody extends State<MyAppContribute>{
           ),
           backgroundColor: Theme.of(context).primaryColor,
         ),
-        body: Container(
-          margin: EdgeInsets.all(20),
-          decoration: BoxDecoration(
-            border: Border.all(
-              color: Colors.blue,
-              width: 4,
+        body: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Container(
+            margin: EdgeInsets.all(20),
+            width: double.infinity,
+            height: 800,
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.blue,
+                width: 4,
+              ),
+              borderRadius: BorderRadius.circular(10),
             ),
-            borderRadius: BorderRadius.circular(10),
-          ),
-        )
-      ),
+            child: Column(
+              children: <Widget>[
+                const SizedBox(height: 20),
+                Container(
+                  padding: EdgeInsets.only(left: 30, right: 30,top: 0,bottom: 20),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Theme.of(context).colorScheme.inversePrimary,
+                      width: 4,
+                    ),
+                    borderRadius: BorderRadius.circular(23),
+                    color: Colors.purple
+                  ),
+                  child: const Text(
+                    'ខ្លាឃ្លោក',
+                    style: TextStyle(
+                      fontSize: 100,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 150),
+                Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        padding: EdgeInsets.only(left: 10, right: 10),
+                        child: Image.asset(
+                          '../assets/images/${ran1[ran1Index]}.jpg',
+                          width: 100,
+                          height: 100,
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.only(left: 10, right: 10),
+                        child: Image.asset(
+                          '../assets/images/${ran2[ran2Index]}.jpg',
+                          width: 100,
+                          height: 100,
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.only(left: 10, right: 10),
+                        child: Image.asset(
+                          '../assets/images/${ran3[ran3Index]}.jpg',
+                          width: 100,
+                          height: 100,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 150),
+                ElevatedButton(
+                  onPressed: () {
+                    RandomNumber();
+                  },
+                  child: const Text('ក្រឡុក'),
+                ),         
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    restart();
+                  },
+                  child: const Text('បិទចាន'),
+                ),         
+              ],
+            )
+          )
+        ),
+      )
     );
   }
 }
